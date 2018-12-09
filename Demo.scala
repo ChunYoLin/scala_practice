22204c2a8aac3bd42b69aa789af70f0dcb73cd6c
Chunyolin <r04922108@csie.ntu.edu.tw>
2018-12-09 21:32:22 +0800

    add: closure example in Demo

--- Demo.scala
+++ Demo.scala
@@ -6,5 +6,10 @@ object Demo
   def main(args: Array[String])
   {
     4 times (println("hello"), 5);
+    var factor = 3;
+    val multiplier = (i: Int) => i * factor
+    println("value: " + multiplier(3))
+    factor = 5;
+    println("value: " + multiplier(3))
   }
 }
